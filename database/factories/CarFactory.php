f3b3dfee4c8b2f5073abc4a9a631c66cc4a0b7cc
Milica <magiflowerchild@gmial.com>
2022-03-29 18:21:46 +0200

    added cars table and model

---
+++ database/factories/CarFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Car>
+ */
+class CarFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'brand' => $this->faker->randomElement(['audi', 'bmw', 'tesla']),
+            'model' =>  Str::random(10),
+            'year' => $this->faker->numberBetween(1960, 2022),
+            'max_speed' => $this->faker->numberBetween(0, 500),
+            'is_automatic' => $this->faker->boolean(),
+            'engine' => $this->faker->randomElement(['diesel', 'electric', 'petrol', 'hybird']),
+            'number_of_doors' =>  $this->faker->numberBetween(2, 10),
+
+        ];
+    }
+}
--- database/seeders/CarSeeder.php
+++ database/seeders/CarSeeder.php
@@ -5,16 +5,15 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class CarSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-        $this->call(CarSeeder::class);
+        \App\Models\Car::factory(10)->create();
     }
 }
